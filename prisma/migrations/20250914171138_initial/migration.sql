2ebf8925b0e8844b9b1ca8a0d47f1770920f3402
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-09-14 18:51:48 -0300

    feat: remove initial migration files and update TipoAnalise model to support multiple Amostra relations

--- prisma/migrations/20250914171138_initial/migration.sql
+++ prisma/migrations/20250914171138_initial/migration.sql
@@ -4,10 +4,10 @@ CREATE TABLE `User` (
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `phone` VARCHAR(191) NULL DEFAULT '',
-    `area` VARCHAR(191) NULL DEFAULT '',
-    `funcao` VARCHAR(191) NULL DEFAULT '',
-    `authorization` BOOLEAN NULL DEFAULT false,
+    `phone` VARCHAR(191) NOT NULL DEFAULT '',
+    `area` VARCHAR(191) NOT NULL DEFAULT '',
+    `funcao` VARCHAR(191) NOT NULL DEFAULT '',
+    `authorization` BOOLEAN NOT NULL DEFAULT false,
     `role` ENUM('Usuário', 'Operador', 'Administrador') NOT NULL DEFAULT 'Usuário',
     `passwordResetToken` VARCHAR(191) NULL,
     `passwordResetExpires` DATETIME(3) NULL,
@@ -37,7 +37,6 @@ CREATE TABLE `TipoAnalise` (
     `classe` VARCHAR(191) NOT NULL DEFAULT '',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `amostraId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -47,8 +46,8 @@ CREATE TABLE `ParametrosAnalise` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `tipoAnaliseId` INTEGER NOT NULL,
     `descricao` VARCHAR(191) NOT NULL DEFAULT '',
-    `unidadeMedida` VARCHAR(191) NULL DEFAULT '',
-    `unidadeResultado` VARCHAR(191) NULL DEFAULT '',
+    `unidadeMedida` VARCHAR(191) NOT NULL DEFAULT '',
+    `unidadeResultado` VARCHAR(191) NOT NULL DEFAULT '',
     `casasDecimais` INTEGER NOT NULL DEFAULT 0,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
@@ -105,9 +104,9 @@ CREATE TABLE `OrdemServico` (
     `status` ENUM('Aguardando Autorização', 'Autorizada', 'Em Execução', 'Finalizada', 'Cancelada') NOT NULL DEFAULT 'Aguardando Autorização',
     `dataRecepcao` VARCHAR(191) NOT NULL DEFAULT '',
     `prazoInicioFim` VARCHAR(191) NOT NULL DEFAULT '',
-    `progresso` VARCHAR(191) NULL DEFAULT '',
-    `observacao` VARCHAR(191) NULL DEFAULT '',
-    `revisorId` VARCHAR(191) NULL DEFAULT '',
+    `progresso` INTEGER NOT NULL DEFAULT 0,
+    `observacao` VARCHAR(191) NOT NULL DEFAULT '',
+    `revisorId` VARCHAR(191) NOT NULL DEFAULT '',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -130,8 +129,8 @@ CREATE TABLE `LaboratorioExterno` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(191) NOT NULL,
     `endereco` JSON NOT NULL,
-    `telefone` VARCHAR(191) NULL DEFAULT '',
-    `email` VARCHAR(191) NULL DEFAULT '',
+    `telefone` VARCHAR(191) NOT NULL DEFAULT '',
+    `email` VARCHAR(191) NOT NULL DEFAULT '',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -142,7 +141,6 @@ CREATE TABLE `LaboratorioExterno` (
 CREATE TABLE `AmostraLabExterno` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `amostraName` VARCHAR(191) NOT NULL,
-    `elementosAnalisados` JSON NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -154,9 +152,25 @@ CREATE TABLE `RemessaLabExterno` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `data` VARCHAR(191) NOT NULL,
     `destinoId` INTEGER NOT NULL,
-    `amostras` JSON NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AmostraAnaliseExterna` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `amostraName` VARCHAR(191) NOT NULL,
+    `subIdentificacao` VARCHAR(191) NOT NULL DEFAULT '',
+    `dataInicio` VARCHAR(191) NOT NULL,
+    `dataFim` VARCHAR(191) NOT NULL,
+    `elementosSolicitados` JSON NOT NULL,
+    `elementosAnalisados` JSON NULL,
+    `analiseConcluida` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `remessaLabExternoId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -170,20 +184,35 @@ CREATE TABLE `_ConfiguracaoAnaliseToParametrosAnalise` (
     INDEX `_ConfiguracaoAnaliseToParametrosAnalise_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `_AmostraToTipoAnalise` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_AmostraToTipoAnalise_AB_unique`(`A`, `B`),
+    INDEX `_AmostraToTipoAnalise_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_AmostraLabExternoToElementoQuimico` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_AmostraLabExternoToElementoQuimico_AB_unique`(`A`, `B`),
+    INDEX `_AmostraLabExternoToElementoQuimico_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Notifications` ADD CONSTRAINT `Notifications_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `TipoAnalise` ADD CONSTRAINT `TipoAnalise_amostraId_fkey` FOREIGN KEY (`amostraId`) REFERENCES `Amostra`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ParametrosAnalise` ADD CONSTRAINT `ParametrosAnalise_tipoAnaliseId_fkey` FOREIGN KEY (`tipoAnaliseId`) REFERENCES `TipoAnalise`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ParametrosAnalise` ADD CONSTRAINT `ParametrosAnalise_tipoAnaliseId_fkey` FOREIGN KEY (`tipoAnaliseId`) REFERENCES `TipoAnalise`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ConfiguracaoAnalise` ADD CONSTRAINT `ConfiguracaoAnalise_tipoAnaliseId_fkey` FOREIGN KEY (`tipoAnaliseId`) REFERENCES `TipoAnalise`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ConfiguracaoAnalise` ADD CONSTRAINT `ConfiguracaoAnalise_tipoAnaliseId_fkey` FOREIGN KEY (`tipoAnaliseId`) REFERENCES `TipoAnalise`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Amostra` ADD CONSTRAINT `Amostra_numeroOs_fkey` FOREIGN KEY (`numeroOs`) REFERENCES `OrdemServico`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Amostra` ADD CONSTRAINT `Amostra_numeroOs_fkey` FOREIGN KEY (`numeroOs`) REFERENCES `OrdemServico`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Amostra` ADD CONSTRAINT `Amostra_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -195,7 +224,22 @@ ALTER TABLE `OrdemServico` ADD CONSTRAINT `OrdemServico_solicitanteId_fkey` FORE
 ALTER TABLE `RemessaLabExterno` ADD CONSTRAINT `RemessaLabExterno_destinoId_fkey` FOREIGN KEY (`destinoId`) REFERENCES `LaboratorioExterno`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `AmostraAnaliseExterna` ADD CONSTRAINT `AmostraAnaliseExterna_remessaLabExternoId_fkey` FOREIGN KEY (`remessaLabExternoId`) REFERENCES `RemessaLabExterno`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `_ConfiguracaoAnaliseToParametrosAnalise` ADD CONSTRAINT `_ConfiguracaoAnaliseToParametrosAnalise_A_fkey` FOREIGN KEY (`A`) REFERENCES `ConfiguracaoAnalise`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_ConfiguracaoAnaliseToParametrosAnalise` ADD CONSTRAINT `_ConfiguracaoAnaliseToParametrosAnalise_B_fkey` FOREIGN KEY (`B`) REFERENCES `ParametrosAnalise`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AmostraToTipoAnalise` ADD CONSTRAINT `_AmostraToTipoAnalise_A_fkey` FOREIGN KEY (`A`) REFERENCES `Amostra`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AmostraToTipoAnalise` ADD CONSTRAINT `_AmostraToTipoAnalise_B_fkey` FOREIGN KEY (`B`) REFERENCES `TipoAnalise`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AmostraLabExternoToElementoQuimico` ADD CONSTRAINT `_AmostraLabExternoToElementoQuimico_A_fkey` FOREIGN KEY (`A`) REFERENCES `AmostraLabExterno`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AmostraLabExternoToElementoQuimico` ADD CONSTRAINT `_AmostraLabExternoToElementoQuimico_B_fkey` FOREIGN KEY (`B`) REFERENCES `ElementoQuimico`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
