5f4cc6055ab1895a2b2010ae44f4af6affd7eae8
Rafael Moraes <133725904+Rafael01Gx@users.noreply.github.com>
2025-10-12 21:10:55 -0300

    Update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,22 @@
-
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const passwordPlain = 'R87559301@lr'; 
+  const passwordPlain = 'Senha@123'; 
   const salt = await bcrypt.genSalt(15);
   const hashedPassword = await bcrypt.hash(passwordPlain, salt);
 
   await prisma.user.upsert({
-    where: { email: 'rafael_junio_moraes@hotmail.com' },
+    where: { email: 'admin@admin.com' },
     update: {},
     create: {
       id: 'af309495-f8b0-4c5c-90b3-772bac712403',
-      name: 'Rafael',
-      email: 'rafael_junio_moraes@hotmail.com',
+      name: 'Admin',
+      email: 'admin@admin.com',
       password: hashedPassword,
-      phone: '31992482029',
+      phone: '5599999999',
       area: 'GAPSI',
       funcao: 'Laboratorista',
       authorization: true,
