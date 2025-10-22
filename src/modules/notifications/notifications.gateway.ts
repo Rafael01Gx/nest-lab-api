a163c9854f0cde6ef07928e1e94852091a1bc33e
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-10-21 21:20:23 -0300

    refactor: correct WebSocketGateway namespace from '/notificacoes' to '/notifications'

--- src/modules/notifications/notifications.gateway.ts
+++ src/modules/notifications/notifications.gateway.ts
@@ -9,7 +9,7 @@ import { Server, Socket } from 'socket.io';
 import { JwtService } from '@nestjs/jwt';
 import { AuthenticatedSocket } from './interfaces/authenticated-socket.interface';
 @WebSocketGateway({
-  namespace: '/notificacoes',
+  namespace: '/notifications',
   transports: ['websocket', 'polling'],
 })
 export class NotificationsGateway
